fde68dd2ae9281b0951739c01d9aa339ced6860e
Sergey <karavaykaravay1995@yandex.ru>
2023-01-31 23:55:44 +0300

    сделал ДЗ за 28.01.23

---
+++ Task 2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает
+// два числа и выдает, какое число большее, а какое меньшее.
+
+Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+int numA = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int numB = int.Parse(Console.ReadLine()!);
+
+if (numA > numB)
+{
+    Console.WriteLine($"Первое число больше второго, следовательно {numA} больше чем {numB}");
+}
+else
+{
+    Console.WriteLine($"Второе число больше первого, следовательно {numB} больше чем {numA}");
+}
---
+++ Task 4/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход
+// три числа и выдает максимальное из этих чисел.
+
+Console.Clear();
+
+Console.WriteLine("Введите число ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число ");
+int num2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число ");
+int num3 = int.Parse(Console.ReadLine()!);
+
+if (num1 > num2)
+{
+    if (num1 > num3)
+    {
+        Console.WriteLine(num1);
+    }
+    else
+    {
+        Console.WriteLine(num3);
+    }
+}
+else if (num2 > num3)
+{
+    Console.WriteLine(num2);
+}
+else
+{
+    Console.WriteLine(num3);
+}
