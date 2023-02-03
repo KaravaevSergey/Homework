a898e2f9a7c766d14ec0adc59090d114054e8af2
Sergey <karavaykaravay1995@yandex.ru>
2023-02-04 02:50:36 +0300

    сделал дз, кроме 13 задачи

---
+++ Task 10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+
+// Console.WriteLine("Напишите любое трезначное число");
+// int a = int.Parse(Console.ReadLine()!);
+// int n = a / 10;
+// int b = n %= 10;
+// Console.WriteLine($"{b}");
+
+int a = new Random().Next(100,999); 
+Console.WriteLine($" {a} ");   
+int n = a / 10;
+int b = n %= 10;
+Console.WriteLine($"{b}");
---
+++ Task 13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+
+Console.WriteLine("Напишите любое число");
+int a = int.Parse(Console.ReadLine()!);
+int n = a % 10;
+if (n = a % 10)
+{
+    Console.WriteLine($" {n} ");
+}
+else
+{
+    Console.WriteLine("нет такой цифры");
+}
